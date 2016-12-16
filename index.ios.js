1b3db4be4742bbca4f2c383a30b165a085a6e275
Harry Engoren <harrison.engoren@gmail.com>
2016-12-16 14:20:19 -0600

    harrys first login page

--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,8 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
+ * Edited by: Harrison Engoren
+ * on my workflow
  */
 
 import React, { Component } from 'react';
@@ -9,7 +11,10 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Button,
+  Alert
 } from 'react-native';
 
 export default class backdoor extends Component {
@@ -17,11 +22,35 @@ export default class backdoor extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Hello mother fucking world
+          welcome to backdoor
         </Text>
+        <View style={styles.container2}>
+          <Text style={styles.login}>
+            login
+          </Text>
+          <TextInput 
+            style={styles.textfield} 
+            defaultValue="username"
+            autoCorrect false
+          />
+          <TextInput
+            style={styles.textfield}
+            defaultValue="password"
+            autoCorrect false
+          />
+          <Button
+            onPress={onButtonPress}
+            title="LOGIN"
+            color='green'
+          />
+        </View>
       </View>
     );
   }
+}
+
+const onButtonPress = () => {
+  Alert.alert('You just entered my backdoor ;)');
 }
 
 const styles = StyleSheet.create({
@@ -29,18 +58,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  container2: {
+    height: 200,
+    width: 300,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 35,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  login: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    marginTop: 50,
+    marginLeft: 30,
+  },
+  textfield: {
+    height: 20,
+    backgroundColor: 'white',
+    marginLeft: 15,
+    marginRight: 15,
+    borderColor: 'lightgray',
+    borderWidth: 1,
   },
 });
+
 
 AppRegistry.registerComponent('backdoor', () => backdoor);
