6487c02f25a68dcc49be18ee6a43fe56b379728b
Harry Engoren <harrison.engoren@gmail.com>
2016-12-19 11:36:58 -0600

    simplified main file

--- main.js
+++ main.js
@@ -34,32 +34,29 @@ export default class backdoor extends Component {
         <Text style={styles.welcome}>
           backdoor
         </Text>
-        <View style={styles.container2}>
-          <TextInput 
-            autoCapitalize="none"
-            style={styles.textfield} 
-            placeholder='username'
-            placeholderTextColor='lightgray'
-            autoCorrect = {false}
-            onChangeText={(text) => this.setState({username:text})}/>
-          <TextInput
-            autoCapitalize="none"
-            placeholder='password'
-            placeholderTextColor='lightgray'
-            style={styles.textfield}
-            autoCorrect = {false}
-            onChangeText = {(text) => this.setState({password:text})}/>
-          <View style={styles.container3}>
-            <Button
-              onPress={onLoginPress}
-              title="LOGIN"
-              color='green'/>
-            <Button
-              onPress={onHelpPress}
-              title="Forgot?"
-              color='orange'/>
-          </View>
-        </View>
+        <TextInput 
+          autoCapitalize="none"
+          style={styles.textfield} 
+          placeholder='username'
+          placeholderTextColor='lightgray'
+          autoCorrect = {false}
+          onChangeText={(text) => this.setState({username:text})}/>
+        <TextInput
+          autoCapitalize="none"
+          placeholder='password'
+          placeholderTextColor='lightgray'
+          style={styles.textfield}
+          secureTextEntry = {true}
+          autoCorrect = {false}
+          onChangeText = {(text) => this.setState({password:text})}/>
+        <Button
+          onPress={onLoginPress}
+          title="LOGIN"
+          color='green'/>
+        <Button
+          onPress={onHelpPress}
+          title="Forgot?"
+          color='orange'/>
       </View>
     );
   }
@@ -79,17 +76,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  container2: {
-    height: 200,
-    width: 300,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container3: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-  },
   welcome: {
     fontSize: 25,
     textAlign: 'center',
@@ -97,16 +83,12 @@ const styles = StyleSheet.create({
     marginBottom: 50,
     margin: 10,
   },
-  login: {
-    textAlign: 'center',
-    marginTop: 50,
-    marginLeft: 10,
-    marginBottom: 2,
-  },
   textfield: {
     height: 20,
     backgroundColor: 'white',
-    margin: 1,
+    marginTop: 1,
+    marginBottom: 1,
+    margin: 20,
     paddingLeft: 2,
     borderColor: 'lightgray',
     borderWidth: 1,
