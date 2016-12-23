9f35a4cd05d686fb7a5858ffc82a43c00891f73f
Harry Engoren <harrison.engoren@gmail.com>
2016-12-23 11:37:04 -0600

    homepage looking better but spacing issues between content and separator

--- Home.js
+++ Home.js
@@ -16,7 +16,7 @@ export default class home extends Component {
 	render() {
 		return (
 				<ListView
-					style = {styles.container} 
+					contentContainerStyle = {styles.container} 
 					dataSource = {this.state.dataSource}
 					renderRow={(data) => <Row {...data} />}
 					renderSeparator={(sectionId, rowId) => <View key={rowId} style = {styles.separator} />}
@@ -32,7 +32,6 @@ const styles = StyleSheet.create({
     paddingLeft: 2,
   },
   separator: {
-  	flex: 1,
   	height: StyleSheet.hairlineWidth,
   	backgroundColor: 'black',
   },
@@ -42,26 +41,32 @@ const musicdata = [
 	{
 		"title" : "Neighbors",
 		"artist" : "J.Cole",
-		"album" : "4 Your Eyez Only"
+		"album" : "4 Your Eyez Only",
+		"picture" : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/4_Your_Eyes_Only.jpg/220px-4_Your_Eyes_Only.jpg"
+
 	},
 	{
 		"title" : "The Guide",
 		"artist" : "Kid Cudi",
-		"album" : "Passion, Pain, & Demon Slayin'"
+		"album" : "Passion, Pain, & Demon Slayin'",
+		"picture" : "https://upload.wikimedia.org/wikipedia/en/thumb/f/fa/Kid_Cudi's_Passion,_Pain_%26_Demon_Slayin'_Cover.jpg/220px-Kid_Cudi's_Passion,_Pain_%26_Demon_Slayin'_Cover.jpg"
 	},
 	{
 		"title" : "Foreplay",
 		"artist" : "Jalen Santoy",
-		"album" : "Foreplay"
+		"album" : "Foreplay",
+		"picture" : "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/4_Play_(Cold_Sweat_album).jpg/220px-4_Play_(Cold_Sweat_album).jpg"
 	},
 	{
 		"title" : "Best Me",
 		"artist" : "Sylvan LaCue",
-		"album" : "Best Me"
+		"album" : "Best Me",
+		"picture" : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Far_From_Familiar.jpg/220px-Far_From_Familiar.jpg"
 	},
 	{
 		"title" : "Staying Alive",
 		"artist" : "070",
-		"album" : "The 070 Project: Chapter One"
+		"album" : "The 070 Project: Chapter One",
+		"picture" : "https://upload.wikimedia.org/wikipedia/en/thumb/f/ff/Projectxepcover.png/220px-Projectxepcover.png"
 	},
 ];
--- row.js
+++ row.js
@@ -3,15 +3,17 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 
 const Row = (props) => (
 	<View style={styles.container}>
+		<View style={styles.container3}>
 		<Text style = {styles.text}>
 			{`${props.title}`}
 		</Text>
-		<Text style = {styles.text}>
-			{`${props.artist}`}
-		</Text>
-		<Text style = {styles.text}>
-			{`${props.album}`}
-		</Text>
+		<Image source={{ uri: props.picture}} style={styles.photo} />
+		</View>
+		<View style = {styles.container2}>
+			<Text style = {styles.ext}>
+				{`${props.artist}`} • {`${props.album}`}
+			</Text>
+		</View>
 	</View>
 	);
 
@@ -19,19 +21,36 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		padding: 12,
+		flexDirection: 'column',
+		backgroundColor: 'limegreen',
+	},
+	container2: {
+		flex: 1,
 		flexDirection: 'row',
-		alignItems: 'center',
+		backgroundColor:'lightblue',
+	},
+	container3: {
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor:'pink',
+
 	},
 	text: {
 		flex: 1,
 		marginLeft: 1,
-		fontSize: 12,
 		justifyContent: 'flex-start',
+		fontWeight: 'bold',
+		fontSize: 18,
+	},
+	ext: {
+		flex: 1,
+		marginLeft: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-between',
 	},
 	photo: {
 		height: 40,
 		width: 40,
-		borderRadius: 20,
 	},
 });
 
